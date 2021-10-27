1a648017b51e45e9efde263f347949bcdf74455a
Raymond An <raymondan100@gmail.com>
2021-10-27 13:50:39 -0700

    Added more endpoints
    Added getAirlinePart and Change Part Owner endpoint and changed naming for api route

--- fabcar/HFRest/src/main/java/com/raymond/HFRest/MyResource.java
+++ fabcar/HFRest/src/main/java/com/raymond/HFRest/MyResource.java
@@ -3,18 +3,36 @@ package com.raymond.HFRest;
 import jakarta.ws.rs.GET;
 import org.example.*;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("cars")
+@Path("parts")
 public class MyResource {
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getParts() {
 		ClientApp clientApp = new ClientApp();
-		return clientApp.getCars();
+		return clientApp.getAllAirlineParts();
     }
+    
+    @GET
+    @Path("{partName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getCurrentPart(@PathParam("partName") String partName) {
+		ClientApp clientApp = new ClientApp();
+		return clientApp.queryAirlinePart(partName);
+    }
+    
+//    @GET
+//    @Path("{newOwner}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String changePartOwner(@PathParam("airlinePart") String airlinePart, @PathParam("newOwner") String newOwner) {
+//		ClientApp clientApp = new ClientApp();
+//		return clientApp.changePartOwner(airlinePart, newOwner);
+//    }
+    
 }
