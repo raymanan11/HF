f9efb655e919317ac4ae030ea7dc5a47c2afad0c
Raymond An <raymondan100@gmail.com>
2021-10-26 22:08:53 -0700

    Update Fabcar go class
    This class is what is used when looking for smart contracts and initializing the ledger so I just edited it to be airline part instead of car

--- chaincode/fabcar/go/fabcar.go
+++ chaincode/fabcar/go/fabcar.go
@@ -12,43 +12,43 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// SmartContract provides functions for managing a car
+// SmartContract provides functions for managing a airline part
 type SmartContract struct {
 	contractapi.Contract
 }
 
-// Car describes basic details of what makes up a car
-type Car struct {
-	Make   string `json:"make"`
-	Model  string `json:"model"`
-	Colour string `json:"colour"`
+// Car describes basic details of what makes up a airline part
+type AirlinePart struct {
+	Name   string `json:"name"`
+	IsDefect  bool `json:"isDefect"`
+	SerialNumber string `json:"serialNumber"`
 	Owner  string `json:"owner"`
 }
 
 // QueryResult structure used for handling result of query
 type QueryResult struct {
 	Key    string `json:"Key"`
-	Record *Car
+	Record *AirlinePart
 }
 
-// InitLedger adds a base set of cars to the ledger
+// InitLedger adds a base set of airline parts to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	cars := []Car{
-		Car{Make: "Toyota", Model: "Prius", Colour: "blue", Owner: "Tomoko"},
-		Car{Make: "Ford", Model: "Mustang", Colour: "red", Owner: "Brad"},
-		Car{Make: "Hyundai", Model: "Tucson", Colour: "green", Owner: "Jin Soo"},
-		Car{Make: "Volkswagen", Model: "Passat", Colour: "yellow", Owner: "Max"},
-		Car{Make: "Tesla", Model: "S", Colour: "black", Owner: "Adriana"},
-		Car{Make: "Peugeot", Model: "205", Colour: "purple", Owner: "Michel"},
-		Car{Make: "Chery", Model: "S22L", Colour: "white", Owner: "Aarav"},
-		Car{Make: "Fiat", Model: "Punto", Colour: "violet", Owner: "Pari"},
-		Car{Make: "Tata", Model: "Nano", Colour: "indigo", Owner: "Valeria"},
-		Car{Make: "Holden", Model: "Barina", Colour: "brown", Owner: "Shotaro"},
+	airlineParts := []AirlinePart{
+		AirlinePart{Name: "Flight Controls", IsDefect: false, SerialNumber: "E96GJE93D", Owner: "United Airlines"},
+		AirlinePart{Name: "Landing Gear", IsDefect: false, SerialNumber: "U46834HJ3", Owner: "American Airlines"},
+		AirlinePart{Name: "Fuselage", IsDefect: true, SerialNumber: "FOIE463U2", Owner: "Delta"},
+		AirlinePart{Name: "Rudder Pedals", IsDefect: false, SerialNumber: "DFU9436OB", Owner: "Spirit"},
+		AirlinePart{Name: "Instrument Panels", IsDefect: false, SerialNumber: "FJE582KFD3", Owner: "Frontier"},
+		AirlinePart{Name: "Engine", IsDefect: true, SerialNumber: "DFJRO895D", Owner: "Alaska Airlines"},
+		AirlinePart{Name: "Wings", IsDefect: false, SerialNumber: "RID5569D2", Owner: "Southwest Airlines"},
+		AirlinePart{Name: "Rudders", IsDefect: false, SerialNumber: "TOIE835D3", Owner: "JetBlue"},
+		AirlinePart{Name: "Vertical Stabalizer", IsDefect: false, SerialNumber: "TI45GMD32W", Owner: "Hawaiian Airlines"},
+		AirlinePart{Name: "Overhead Panel", IsDefect: true, SerialNumber: "EKLF8534H", Owner: "Allegiant Air"},
 	}
 
-	for i, car := range cars {
-		carAsBytes, _ := json.Marshal(car)
-		err := ctx.GetStub().PutState("CAR"+strconv.Itoa(i), carAsBytes)
+	for i, parts := range airlineParts {
+		partsAsBytes, _ := json.Marshal(parts)
+		err := ctx.GetStub().PutState("PART"+strconv.Itoa(i), partsAsBytes)
 
 		if err != nil {
 			return fmt.Errorf("Failed to put to world state. %s", err.Error())
@@ -58,40 +58,40 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	return nil
 }
 
-// CreateCar adds a new car to the world state with given details
-func (s *SmartContract) CreateCar(ctx contractapi.TransactionContextInterface, carNumber string, make string, model string, colour string, owner string) error {
-	car := Car{
-		Make:   make,
-		Model:  model,
-		Colour: colour,
+// CreateAirlinePart adds a new airline part to the world state with given details
+func (s *SmartContract) CreateAirlinePart(ctx contractapi.TransactionContextInterface, airlinePartNumber string, name string, isDefect bool, serialNumber string, owner string) error {
+	airlinePart := AirlinePart{
+		Name:   name,
+		IsDefect:  isDefect,
+		SerialNumber: serialNumber,
 		Owner:  owner,
 	}
 
-	carAsBytes, _ := json.Marshal(car)
+	airlinePartAsBytes, _ := json.Marshal(airlinePart)
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	return ctx.GetStub().PutState(airlinePartNumber, airlinePartAsBytes)
 }
 
-// QueryCar returns the car stored in the world state with given id
-func (s *SmartContract) QueryCar(ctx contractapi.TransactionContextInterface, carNumber string) (*Car, error) {
-	carAsBytes, err := ctx.GetStub().GetState(carNumber)
+// QueryAirlinePart returns the airline part stored in the world state with given id
+func (s *SmartContract) QueryAirlinePart(ctx contractapi.TransactionContextInterface, airlinePartNumber string) (*AirlinePart, error) {
+	airlinePartAsBytes, err := ctx.GetStub().GetState(airlinePartNumber)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read from world state. %s", err.Error())
 	}
 
-	if carAsBytes == nil {
-		return nil, fmt.Errorf("%s does not exist", carNumber)
+	if airlinePartAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", airlinePartNumber)
 	}
 
-	car := new(Car)
-	_ = json.Unmarshal(carAsBytes, car)
+	airlinePart := new(AirlinePart)
+	_ = json.Unmarshal(airlinePartAsBytes, airlinePart)
 
-	return car, nil
+	return airlinePart, nil
 }
 
-// QueryAllCars returns all cars found in world state
-func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
+// QueryAllAirlineParts returns all airline parts found in world state
+func (s *SmartContract) QueryAllAirlineParts(ctx contractapi.TransactionContextInterface) ([]QueryResult, error) {
 	startKey := ""
 	endKey := ""
 
@@ -111,29 +111,29 @@ func (s *SmartContract) QueryAllCars(ctx contractapi.TransactionContextInterface
 			return nil, err
 		}
 
-		car := new(Car)
-		_ = json.Unmarshal(queryResponse.Value, car)
+		airlinePart := new(AirlinePart)
+		_ = json.Unmarshal(queryResponse.Value, airlinePart)
 
-		queryResult := QueryResult{Key: queryResponse.Key, Record: car}
+		queryResult := QueryResult{Key: queryResponse.Key, Record: airlinePart}
 		results = append(results, queryResult)
 	}
 
 	return results, nil
 }
 
-// ChangeCarOwner updates the owner field of car with given id in world state
-func (s *SmartContract) ChangeCarOwner(ctx contractapi.TransactionContextInterface, carNumber string, newOwner string) error {
-	car, err := s.QueryCar(ctx, carNumber)
+// ChangeAirlinePartOwner updates the owner field of airline part with given id in world state
+func (s *SmartContract) ChangeAirlinePartOwner(ctx contractapi.TransactionContextInterface, airlinePartNumber string, newOwner string) error {
+	airlinePart, err := s.QueryAirlinePart(ctx, airlinePartNumber)
 
 	if err != nil {
 		return err
 	}
 
-	car.Owner = newOwner
+	airlinePart.Owner = newOwner
 
-	carAsBytes, _ := json.Marshal(car)
+	airlinePartAsBytes, _ := json.Marshal(airlinePart)
 
-	return ctx.GetStub().PutState(carNumber, carAsBytes)
+	return ctx.GetStub().PutState(airlinePartNumber, airlinePartAsBytes)
 }
 
 func main() {
